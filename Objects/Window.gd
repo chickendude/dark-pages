44f8de54a999a74937eb836f36d14f4196908a4b
chickendude <pokemaster103@gmail.com>
2020-08-02 19:43:28 +0200

    Combine window/windowlight into one scene. Add a "light switch" to turn lights behind window on or off.

--- Objects/Window.gd
+++ Objects/Window.gd
@@ -1,19 +1,23 @@
-extends Area2D
+extends StaticBody2D
 
 export (String) var map_name
 export (float) var x
 export (float) var y
 export (Vector2) var direction
+export (bool) var lights_on
 export (String) var text
 
 const PLAYER_HITBOX_HEIGHT = 12
 
 func _ready():
 	var _e = connect("body_entered", self, "_on_body_entered")
+	$Area2D.connect("body_entered", self, "_on_body_entered")
+	if lights_on:
+		$Sprite.texture = load("res://data/sprites/objects/window_light.png")
 
 func _on_body_entered(body):
+	print(body.name)
 	if text:
-		Event.push_player_back(Vector2(0, position.y + PLAYER_HITBOX_HEIGHT))
-		Event.display_text("The window's stuck... can't get it open.")
+		Event.display_text(text)
 	else:
 		Event.load_map(map_name, x, y, direction)

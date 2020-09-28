359b1487b0095dbe9e8f6ab85f202bd8dff57dc9
chickendude <pokemaster103@gmail.com>
2020-09-28 17:28:52 -0400

    queue_free Pause PathAction when done with action.

--- Paths/Actions/Pause.gd
+++ Paths/Actions/Pause.gd
@@ -7,4 +7,5 @@ export var wait_time : int
 func _on_body_entered(body : StepdadMonster):
     emit_signal("pause")
     yield(get_tree().create_timer(wait_time), "timeout")
-    emit_signal("resume")    
+    emit_signal("resume")
+    queue_free()
